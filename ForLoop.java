49514d79d8318a0f5231b6da7f99b3534250038c
HarishSharma1991 <47175133+HarishSharma1991@users.noreply.github.com>
2019-01-30 16:18:16 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,12 @@
+package Forloop;
+public class ForLoop {
+    public static void main(String[] args)
+    {
+        int no;
+        for( no=2; no<=20; no=no+2)
+        {
+            System.out.print(no +" ");
+        }
+    }
+}
+
